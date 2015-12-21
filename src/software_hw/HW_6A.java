587fe7c12ac2175728dd4c74f3f1ff2e353b95ef
MengPing <owoa01@gmail.com>
2015-12-21 19:04:11 +0800

    ver 1 需要修改print方法

---
+++ src/software_hw/HW_6A.java
@@ -0,0 +1,23 @@
+package software_hw;
+
+
+abstract class test{
+	abstract void print();
+}
+class Question extends test{
+	String description;
+	public Question(String desc){
+		this.description = desc;
+	}
+	public void print(){
+		System.out.println(description);
+	}
+}
+public class HW_6A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
